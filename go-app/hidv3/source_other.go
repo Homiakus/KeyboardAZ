aaf8f08ac53ff31e24e6ef7af5135f1341d82063
Denis <140589513+Homiakus@users.noreply.github.com>
2026-08-27 19:47:47 +0300

    chore: keep HID v3 options API cross-platform

--- go-app/hidv3/source_other.go
+++ go-app/hidv3/source_other.go
@@ -2,7 +2,11 @@
 
 package hidv3
 
-import "hapticpad-go-app/device"
+import (
+	"hapticpad-go-app/device"
+	"hapticpad-go-app/protocol"
+	"hapticpad-go-app/telemetry"
+)
 
 func Discover() ([]Candidate, error) {
 	return nil, ErrNotSupported
@@ -12,10 +16,28 @@ func Open(device.Identity) (*Reader, error) {
 	return nil, ErrNotSupported
 }
 
+func OpenWithRecorder(device.Identity, telemetry.Recorder) (*Reader, error) {
+	return nil, ErrNotSupported
+}
+
+func OpenWithOptions(device.Identity, OpenOptions) (*Reader, error) {
+	return nil, ErrNotSupported
+}
+
 func OpenCandidate(Candidate) (*Reader, error) {
+	return nil, ErrNotSupported
+}
+
+func OpenCandidateWithRecorder(Candidate, telemetry.Recorder) (*Reader, error) {
+	return nil, ErrNotSupported
+}
+
+func OpenCandidateWithOptions(Candidate, OpenOptions) (*Reader, error) {
 	return nil, ErrNotSupported
 }
 
 type Reader struct{}
 
-func (*Reader) Close() error { return nil }
+func (*Reader) Messages() <-chan protocol.Event { return nil }
+func (*Reader) Errors() <-chan error            { return nil }
+func (*Reader) Close() error                    { return nil }
